b43a47ba109b661994cf0ed6346e04504c7964b4
Fabian C. Moss <fabianmoss@gmail.com>
2022-04-10 15:57:27 +0200

    adapt test to new Scales class

--- mscales/tests/test_examples.py
+++ mscales/tests/test_examples.py
@@ -1,6 +1,8 @@
-# from ..scales import Scales
+from ..scales import Scales
 
 
-# def test_generate_scales():
-#     for c in range(15):
-#         assert 2**c == Scales(cardinality=c).all().shape[0]
+def test_generate_scales():
+    for c in range(15):
+        s = Scales(cardinality=c)
+        scales = s.all()
+        assert 2**c == scales.n_scales
