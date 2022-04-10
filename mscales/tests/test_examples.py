b4fe91663f6a287c5f39202ec4476dd36cb0e87c
Fabian C. Moss <fabianmoss@gmail.com>
2022-04-10 16:01:03 +0200

    aaah

--- mscales/tests/test_examples.py
+++ mscales/tests/test_examples.py
@@ -4,5 +4,5 @@ from ..scales import Scales
 def test_generate_scales():
     for c in range(15):
         s = Scales(cardinality=c)
-        scales = s.all()
-        assert 2**c == scales.n_scales
+        _ = s.all()
+        assert 2**c == s.n_scales
