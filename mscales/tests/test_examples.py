8a8e3cce0bf02e318a13d755010115d1c8c344e6
Fabian C. Moss <fabianmoss@gmail.com>
2022-05-16 11:48:43 +0200

    fix test

--- mscales/tests/test_examples.py
+++ mscales/tests/test_examples.py
@@ -3,6 +3,6 @@ from ..scales import Scales
 
 def test_generate_scales():
     for c in range(15):
-        s = Scales(cardinality=c)
+        s = Scales(c=c)
         _ = s.all()
         assert 2**c == s.n_scales
