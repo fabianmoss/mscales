49f728cd7354b851bf8b35dcc21cb58d0d00c645
Fabian C. Moss <fabianmoss@gmail.com>
2022-05-17 17:38:09 +0200

    implement simple radar plot

--- mscales/plots.py
+++ mscales/plots.py
@@ -20,3 +20,21 @@ def plot_scale(s, save=False):
     if save:
         plt.savefig(save)
     plt.show()
+
+
+def plot_polar(s, save=False):
+
+    _, ax = plt.subplots(subplot_kw={"projection": "polar"})
+    ax.set_theta_direction(-1)
+    ax.set_theta_offset(np.pi / 2.0)
+    # ax.set_xticks(np.arange(s.shape[0]))
+    # ax.set_xticklabels(np.arange(s.shape[0]))
+    # ax.set_xticks(np.linspace(0, 2* np.pi* 12, 12, endpoint=False))
+    lines, labels = plt.thetagrids(np.linspace(0, 360, 12, endpoint=False), np.arange(12))
+
+    for i, k in enumerate(s):
+        ax.plot([0, i], [0, k], marker="o", c="k", markersize=8)
+
+    if save:
+        plt.savefig(save)
+    plt.show()
