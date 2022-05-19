e655972ec760cb2457af6a23affebb5069bfe4ac
Fabian C. Moss <fabianmoss@gmail.com>
2022-05-19 19:51:16 +0200

    implement normal form but not yet prime form

--- mscales/pc-sets.py
+++ mscales/pc-sets.py
@@ -2,27 +2,29 @@ import numpy as np
 
 
 class PitchClassSet:
-    def __init__(self, pcset: set = {}, c: int = 12):
-
+    def __init__(self, pcset, c: int = 12):
         self.c = c
-        self.pcset = {p % self.c for p in pcset}
 
-    def sum(self) -> int:
-        return sum(self.pcset)
+        if isinstance(pcset, set):
+            self.pcs = np.array(list({p % self.c for p in pcset}))
+        elif isinstance(pcset, list):
+            self.pcs = np.array(pcset)
+        elif isinstance(pcset, np.ndarray):
+            self.pcs = pcset
+        else:
+            raise TypeError("I don't recognize the pitch-class input.")
 
-    def transpose(self, n: int) -> set:
-        return {(p + n) % self.c for p in self.pcset}
+    def transpose(self, n: int):
+        return PitchClassSet((self.pcs + n) % self.c)
 
-    def invert(self, n: int = 0) -> set:
-        return {(n - p) % self.c for p in self.pcset}
+    def invert(self, n: int = 0):
+        return PitchClassSet((n - self.pcs) % self.c)
 
     def complement(self) -> set:
-        return {p for p in range(self.c)}.difference(self.pcset)
+        return PitchClassSet(np.setdiff1d(np.arange(self.c), self.pcs))
 
     def normal_form(self):
-
-        s = sorted(self.pcset)
-
+        s = sorted(self.pcs)
         rotations = np.asarray([s[i : len(s)] + s[0:i] for i in range(len(s))])
 
         for i in range(rotations.shape[1] - 1, 0, -1):
@@ -30,30 +32,35 @@ class PitchClassSet:
             mask = (rotations[:, i] - rotations[:, 0]) % self.c == min_diff
 
             if np.array_equal(rotations, rotations[mask]):
-                return rotations[0, :]
+                return PitchClassSet(rotations[0, :])
             elif rotations.shape[0] > 1:
                 rotations = rotations[mask]
                 pass
             else:
-                return rotations.flatten()
+                return PitchClassSet(rotations.flatten())
+
+    def most_compact(self, arr):
+        for i in range(arr.shape[1] - 1, 0, -1):
+            min_diff = min([(r[i] - r[0]) % self.c for r in arr])
+            mask = (arr[:, i] - arr[:, 0]) % self.c == min_diff
+
+            if np.array_equal(arr, arr[mask]):
+                return PitchClassSet(arr[0, :])
+            elif arr.shape[0] > 1:
+                arr = arr[mask]
+                pass
+            else:
+                return PitchClassSet(arr.flatten())
 
     def prime_form(self):
-        # can't use `self.transpose` and `self.invert` here, because it operates on sets
-        # but `self.normal_form`` returns array. Those functions need to be updated to work
-        # on arrays as well.
 
-        normal = self.normal_form()
-        normal -= normal[0] % self.c
+        t = self.normal_form()
+        t = t.transpose(-t.pcs[0])
 
-        normal_inverted = -normal % self.c
-        normal_inverted -= normal_inverted[0] % self.c
+        i = t.invert().normal_form()
+        i = i.transpose(-i.pcs[0])
 
-        return self.normal_form()
-
-        if sum(normal) < sum(normal_inverted):
-            prime = np.sort(normal % self.c)
-        else:
-            prime = np.sort(normal_inverted % self.c)
+        return t.pcs, i.pcs
 
         # The following special cases were taken from https://ianring.com/
         # TODO: Implement special cases!
@@ -67,27 +74,26 @@ class PitchClassSet:
         # // 7-20      (0125679)         (0124789)
         # // 8-26      (0134578T)        (0124579T)
 
-        return tuple(prime)
-
     def interval_vector(self):
         raise NotImplementedError
 
+    def sum(self) -> int:
+        return sum(self.pcs)
+
     def info(self):
-        print("pc set\t:", pcset.pcset)
-        print("compl.\t:", pcset.complement())
-        print("sum\t:", pcset.sum())
-        print("transp.\t:", pcset.transpose(2))
-        print("inv.\t:", pcset.invert())
-        print("T2I\t:", pcset.invert(2))
-        print("NF\t:", pcset.normal_form())
-
-
-#        print("PF\t:", pcset.prime_form())
+        print("pc set\t:", pcset.pcs)
+        print("compl.\t:", pcset.complement().pcs)
+        print("transp.\t:", pcset.transpose(2).pcs)
+        print("inv.\t:", pcset.invert().pcs)
+        print("T2I\t:", pcset.invert(2).pcs)
+        print("NF\t:", pcset.normal_form().pcs)
+        print("PF\t:", pcset.prime_form())
 
 
 if __name__ == "__main__":
 
-    s = {2, 3, 11}
-    # s = {2,3,8,9}
+    s = {3, 11, 2}
+    # s = {8,0,9}
+    # s = {0,2,4,5,7,9,11}
     pcset = PitchClassSet(s)
     pcset.info()
