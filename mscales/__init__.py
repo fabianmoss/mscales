6917237bae9645359e2d3456b49d359f75c90426
Fabian C. Moss <fabianmoss@gmail.com>
2022-05-23 17:57:31 +0200

    add pcsets docs

--- mscales/__init__.py
+++ mscales/__init__.py
@@ -1,6 +1,7 @@
 from ._version import get_versions
 from .scales import Scales
-from .pcsets import PitchClassSet
+from .pcsets import PitchClass, PitchClassInterval, PitchClassSet
+from .plots import plot_barcode, plot_polar
 
 __version__ = get_versions()["version"]
 del get_versions
