dc7fc638a56ff6c7aa5dce8bcbd04b8d5ed3149f
Fabian C. Moss <fabianmoss@gmail.com>
2022-05-23 08:40:44 +0200

    cleanup

--- mscales/__init__.py
+++ mscales/__init__.py
@@ -1,6 +1,6 @@
 from ._version import get_versions
-from .scales import Scales  # pyflakes.ignore
-from .pcsets import PitchClassSet  # pyflakes.ignore
+from .scales import Scales
+from .pcsets import PitchClassSet
 
 __version__ = get_versions()["version"]
 del get_versions
