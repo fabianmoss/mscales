48cc2d469e066090693e01e79a3ee94bda8df3b4
Fabian C. Moss <fabianmoss@gmail.com>
2022-05-24 14:22:56 +0200

    replace tones with pretty_midi

--- mscales/sound.py
+++ mscales/sound.py
@@ -1,151 +1,49 @@
 import numpy as np
 from numpy.random import default_rng
-
-# Use the tones library to synthesize scales: https://pypi.org/project/tones/
-from tones import SINE_WAVE, SAWTOOTH_WAVE, SQUARE_WAVE
-from tones.mixer import Mixer
+import pretty_midi as pm
 
 
-rng = default_rng()
-
-# Specific pitch-class spellings
-NOTES = ["c", "c#", "d", "eb", "e", "f", "f#", "g", "ab", "a", "bb", "b"]
-
-wave_dict = {"SINE_WAVE": SINE_WAVE, "SAWTOOTH_WAVE": SAWTOOTH_WAVE, "SQUARE_WAVE": SQUARE_WAVE}
-
-
-def pc2note(pc):
-    """Converts a pitch class to a spelled note name.
-
-    Parameters
-    ----------
-    pc : int
-        Pitch class (integer between 0 and 11, inclusively)
-
-    Returns
-    -------
-    str
-        Note name (spelling) of pitch class
-    """
-
-    assert pc in range(0, 12)
-
-    d = {p: n for p, n in zip(np.arange(12), NOTES)}
-
-    return d[pc]
-
-
-def scale2notelist(s):
-    """Converts a scale to a list of pitch classes.
-
-    Parameters
-    ----------
-    s : np.array
-        Scale with non-negative entries
-
-    Returns
-    -------
-    list
-        List of note names
-    """
-
-    assert s.shape[0] == 12
-
-    (pcs,) = np.nonzero(s)
-
-    return [pc2note(pc) for pc in pcs]
+rng = default_rng(123)
 
 
 def tone_cloud(
     scale,
-    tracks=1,
-    duration=10,
-    rate=5,
-    waveform="SINE_WAVE",
-    sample_rate=44100,
-    amplitude=0.5,
-    attack=0.01,
-    decay=0.1,
-    save_as=None,
+    n_notes: int = 100,
+    note_duration: float = 0.15,
+    velocity: int = 100,
+    instrument_name: str = "Acoustic Grand Piano",
+    save_as: str = None,
 ):
-    """Generate a tone cloud from a note list (specific spellings of pitch classes).
-    NOTE: Currently only works with 12-tet vectors because the `tones` libray only accepts Western note names.
+    starts = np.arange(n_notes) * note_duration  # onsets
+    ends = starts + note_duration  # offsets
 
-    Parameters
-    ----------
-    scale : numpy.array
-        Binary numpy array
-    tracks : int, optional
-        Number of tracks that play simultaneously, by default 1
-    duration : float, optional
-        Duration of the synthesized tone cloud in seconds, by default 10
-    rate : int, optional
-        Tones per second, by default 5
-    waveform: str, optional
-        Waveform from `tones` library (one of `SINE_WAVE`, `SQUARE_WAVE`, `SAWTOOTH_WAVE`), by detault `SINE_WAVE`
-    sample_rate : int, optional
-        Sample rate, by default 44100
-        NOTE: This is the sample rate for the audio to be synthesizes, _not_ the
-        sampling rate for the tone cloud
-    amplitude : float, optional
-        Amplitude, by default .5
-    attack : float, optional
-        Attack, by default 0.01
-    decay : float, optional
-        Decay, by default 0.1
-    save_as : str, optional
-        File path to save the wave file, by default None
+    # binary = "".join([str(x) for x in scale])
 
-    Returns
-    -------
-    tones.tone.Sample
-        Array of samples
-    """
+    pitches = [x for x in rng.choice(np.nonzero(scale)[0], size=n_notes)]
+    octaves = rng.choice(np.arange(3, 7), size=n_notes)
+    midi_pitches = [(p + 12 * o) for p, o in list(zip(pitches, octaves))]
 
-    assert wave_dict[waveform] in [SINE_WAVE, SQUARE_WAVE, SAWTOOTH_WAVE]
+    # Create a PrettyMIDI object
+    midi = pm.PrettyMIDI()
 
-    note_list = scale2notelist(scale)
+    # Create an Instrument instance for an instrument
+    assert instrument_name in pm.constants.INSTRUMENT_MAP, f"Instrument must be in {pm.constants.INSTRUMENT_MAP}"
+    # instrument_code = pm.constants.INSTRUMENT_MAP.index(instrument_name)
 
-    # initialize mixer
-    mixer = Mixer(sample_rate, amplitude)
+    program = pm.instrument_name_to_program(instrument_name)
+    instrument = pm.Instrument(program=program)
 
-    # add tracks
-    for i in range(tracks):
-        mixer.create_track(i, wave_dict[waveform], attack=attack, decay=decay)
+    for mp, s, e in zip(midi_pitches, starts, ends):
+        # Create a Note instance for this note, starting at `s` and ending at `e`.
+        note = pm.Note(pitch=mp, velocity=velocity, start=s, end=e)
+        # Add it to instrument
+        instrument.notes.append(note)
 
-        # sample tones from list and add to mixer
-        tones = [np.random.choice(note_list) for _ in range(duration * rate)]
-        for t in tones:
-            # TODO: add non-uniform durations
-            # if duration == "random":
-            #     mixer.add_note(
-            #         i, note=t, octave=np.random.randint(3, 6), duration=rng.exponential(scale=0.25) + 0.0001
-            #     )
-            mixer.add_note(i, note=t, octave=np.random.randint(3, 6), duration=1 / rate)
+    # Add the instrument to the PrettyMIDI object
+    midi.instruments.append(instrument)
 
-    # create samples
-    samples = mixer.mix()
-
-    # save
     if save_as is not None:
-        mixer.write_wav(save_as)
-
-    # return samples
-    return samples  # ipd.Audio(samples, rate=sample_rate)
-
-
-# MINGUS TEST
-# from mingus.containers import Note, NoteContainer
-# from mingus.midi import fluidsynth
-# from mingus.midi.midi_file_out import write_NoteContainer
-
-# # sf = "/home/fmoss/Downloads/FluidR3_GM/FluidR3_GM.sf2"
-# sf = "/home/fmoss/Downloads/nintendo_soundfont.sf2"  # from musical-artifacts.com
-# fluidsynth.init(sf, "alsa")
-# fluidsynth.set_instrument(1, 35, 0)
-
-# if __name__ == "__main__":
-#     m = Note("C#")
-#     m.channel = 1
-#     nc = NoteContainer(m)
-#     write_NoteContainer("test.mid", nc)
+        # filename = f'mid_i{instrument_code}_p{binary}_d{int(note_duration * 1000)}.mid'
+        midi.write(save_as)
+    else:
+        return midi
