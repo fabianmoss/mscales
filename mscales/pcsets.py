15fd58081e8f09c376b70363fc6ce7d76dff7f3e
Fabian Moss <fabianmoss@gmail.com>
2023-07-12 13:00:59 +0200

    plotting

--- mscales/pcsets.py
+++ mscales/pcsets.py
@@ -1,6 +1,7 @@
 import numpy as np
 from itertools import combinations
 from collections.abc import Iterable
+import matplotlib.pyplot as plt
 
 
 class PitchClass:
@@ -73,16 +74,13 @@ class PitchClassSet:
         self.c = c
         self.d = len(pcset)
 
-        if isinstance(pcset, Iterable):
-            pcset = set(pcset)
-
         if isinstance(pcset, str):
             assert all(
                 x in [str(i) for i in range(10)] + ["T"] + ["E"] for x in list(pcset)
             ), "Some pitch classes are not valid."
             self.pcs = np.array([10 if p == "T" else 11 if p == "E" else int(p) for p in list(pcset)])
         elif isinstance(pcset, (Iterable, PitchClassSet)):
-            self.pcs = np.array(list(pcset))
+            self.pcs = np.array([int(p) for p in pcset])
         else:
             raise TypeError(f"I don't recognize the pitch-class input {type(pcset)}.")
 
@@ -116,7 +114,7 @@ class PitchClassSet:
     def complement(self):
         return PitchClassSet(np.setdiff1d(np.arange(self.c), self.pcs))
 
-    def normal_form(self, all=False):
+    def normal_form(self):
         """
         Bring pitch-class set in normal form according to description at:
         https://musictheory.pugetsound.edu/mt21c/NormalForm.html
@@ -128,27 +126,27 @@ class PitchClassSet:
             raise "PitchClassSet is empty!"
         elif len(self.pcs) == 1:
             return self
+        else:
+            rotations = np.array([np.roll(self.pcs, i) for i in range(self.pcs.shape[0])])
+            for length in range(self.d - 1, 0, -1):
+                spans = [(r[-1] - r[0]) % self.c for r in rotations[:, : length + 1]]
+                mask = spans == min(spans)
+                min_span_rotations = rotations[mask]
 
-        rotations = np.array([np.roll(self.pcs, i) for i in range(self.pcs.shape[0])])
-        for length in range(self.d - 1, 0, -1):
-            spans = [(r[-1] - r[0]) % self.c for r in rotations[:, : length + 1]]
-            mask = spans == min(spans)
-            min_span_rotations = rotations[mask]
+                # if there is a tie in the first step and we want to obtain all candidates
+                # if there is only one candidate left
+                if min_span_rotations.shape[0] == 1:
+                    return PitchClassSet(min_span_rotations.flatten())
+                # (if there are more than one)
+                elif min_span_rotations.shape[0] > 1:  # (length == self.d - 1) and
+                    rotations = min_span_rotations
+                else:
+                    raise "Something went wrong!"
 
-            # if there is a tie in the first step and we want to obtain all candidates
-            # (if there are more than one)
-            if (length == self.d - 1) and all and (min_span_rotations.shape[0] > 1):
-                return min_span_rotations
-            # if there is only one candidate left
-            elif min_span_rotations.shape[0] == 1:
-                return PitchClassSet(min_span_rotations.flatten())
-            else:
-                continue
-
-            # if there is an absolute tie, chose the one with smaller first element
-        if min_span_rotations.shape[0] > 1:
-            min_idx = np.argmin(min_span_rotations, axis=0)[0]
-            return PitchClassSet(min_span_rotations[min_idx])
+                # if there is an absolute tie, chose the one with smaller first element
+            if min_span_rotations.shape[0] > 1:
+                min_idx = np.argmin(min_span_rotations, axis=0)[0]
+                return PitchClassSet(min_span_rotations[min_idx])
 
     def prime_form(self):
         """Prime form of the pitch-class set, after Rahn.
@@ -172,25 +170,13 @@ class PitchClassSet:
 
         candidate2 = sorted.normal_form().transpose(-sorted.normal_form().pcs[0])
 
-        if np.less_equal(candidate1.pcs, candidate2.pcs).all():
-            return candidate1
-        elif np.less_equal(candidate2.pcs, candidate1.pcs).all():
-            return candidate2
-        else:
-            return candidate1, candidate2
-
-        # # if there had been more than one candidate for normal form
-
-        # return sorted.normal_form().transpose(-sorted.normal_form().pcs[0]), transposed
-        #     transposed_inverted = transposed_inverted.sort().normal_form()
-        # # if np.array_equal(inverted.pcs, transposed_inverted.pcs):
-        # #     return transposed
-        # if np.less_equal(transposed.pcs, transposed_inverted.pcs).all():
-        #     return transposed
-        # elif np.less_equal(transposed_inverted.pcs, transposed.pcs).all():
-        #     return transposed_inverted
-        # else:
-        #     return "Something went wrong.", (transposed_inverted.pcs, transposed.pcs)
+        try:
+            if np.less_equal(candidate1.pcs, candidate2.pcs).all():
+                return candidate1
+            elif np.less_equal(candidate2.pcs, candidate1.pcs).all():
+                return candidate2
+        except Exception as e:
+            return e
 
         # The following special cases were taken from https://ianring.com/
         # TODO: Implement special cases!
@@ -228,6 +214,61 @@ class PitchClassSet:
     def matrix(self):
         return np.asarray([self.transpose(-i).pcs for i in self.pcs])
 
+    def plot(self, kind="lollipop", save=False):
+
+        if kind == "lollipop":
+            # c = s.shape[0]
+
+            # figure and axis settings
+            _, ax = plt.subplots(subplot_kw={"projection": "polar", "clip_on": False})
+            ax.set_theta_direction(-1)
+            ax.set_theta_zero_location("N")
+            ax.set_yticklabels([])
+            ax.set_ylim(0, 1)
+            plt.thetagrids(np.linspace(0, 360, self.c, endpoint=False), np.arange(self.c))
+
+            # data
+            thetas = [k / self.c * 2 * np.pi for k in np.flatnonzero(self.to_vector())]
+            radii = np.ones(len(thetas))
+
+            stems = ax.stem(thetas, radii, linefmt="k", markerfmt="ok")
+            for st in stems:
+                st.set_clip_on(False)
+            plt.setp(stems, "linewidth", 3)
+            plt.setp(stems[0], "markersize", 10)
+        if kind == "area":
+            # c = s.shape[0]
+
+            # figure and axis settings
+            _, ax = plt.subplots(subplot_kw={"projection": "polar", "clip_on": False})
+            ax.set_theta_direction(-1)
+            ax.set_theta_zero_location("N")
+            ax.set_yticklabels([])
+            ax.set_ylim(0, 1)
+            plt.thetagrids(np.linspace(0, 360, self.c, endpoint=False), np.arange(self.c))
+
+            # data
+            thetas = [k / self.c * 2 * np.pi for k in np.flatnonzero(self.to_vector())]
+            radii = [1 for _ in range(len(thetas))]
+
+            thetas += thetas[:1]
+            radii += radii[:1]
+
+            ax.plot(thetas, radii, c="k", zorder=5)
+            ax.fill(thetas, radii, alpha=0.75, zorder=4)
+
+        elif kind == "bar":
+            _, ax = plt.subplots()
+            ax.bar(np.arange(self.c), self.to_vector(), color="k")
+            ax.set(xlabel="Pitch class", yticks=[], ylim=(0, 1))
+
+        else:
+            print("I don't recognize the plot kind." "Valid values are 'polar' and 'bar'.")
+
+        if save:
+            plt.savefig(save)
+        plt.show()
+
     def info(self):
         print("=" * len(repr(self)))
         print(repr(self))
@@ -257,10 +298,11 @@ class PitchClassSet:
 if __name__ == "__main__":
 
     # test cases from https://musictheory.pugetsound.edu/mt21c/PrimeForm.html
-    # s = {3, 11, 2}
-    # s = {8,0,9}
+    s = {3, 11, 2}
+    s = {8, 0, 9}
     s = "123E"
     s = "17TE"
+    s = "941"
     # s = {11,2,3,7,2}
     # s = {2,3,8,9}
     # s = {0, 2, 4}
@@ -271,6 +313,6 @@ if __name__ == "__main__":
     # s = {6,9,2}
     # s = {7, 10, 1, 5}
     # s = [0, 1, 6, 7, 5, 2, 4, 3, 10, 9, 11, 8]  # 12-tone row
-    pcset = PitchClassSet(s)
 
+    pcset = PitchClassSet(s)
     pcset.info()
