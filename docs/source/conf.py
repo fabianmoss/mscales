12ac768e7f64463e8b8ba8739591abb9caa542dc
Fabian Moss <fabianmoss@gmail.com>
2023-07-12 16:10:59 +0200

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,7 +67,7 @@ templates_path = ["_templates"]
 source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = "index"
+master_doc = "index.rst"
 
 # General information about the project.
 project = "mscales"
